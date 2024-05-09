f52e4741ad7375e6ba7d30f880a1fe67f9d711f3
ebkendall <ebkendal@ncsu.edu>
2024-05-09 16:56:12 -0400

    I think I figured out the problem

--- AJ_Est_test/aj_runfile.r
+++ AJ_Est_test/aj_runfile.r
@@ -7,14 +7,13 @@ exact_time = as.logical(as.numeric(args[1]))
 par_index = list( beta=1:15, misclass=16:19, pi_logit=20:21)
 
 load('DataOut/trueValues.rda')
-# trueValues[10] = trueValues[10] * 2
-# trueValues[5] = trueValues[5] / 2
-# trueValues[par_index$pi_logit] = c(-2,-2)
-trueValues= c(-2.31617310,  -1.28756312,  -1.10116400,  -2.52367543,  -2.10384797,
-              0.27050001, -11.65470594,  -0.49306415,   0.28862090,   0.22731278,
-              -0.39079609,  -0.05894252,  -0.32509646,   0.48631653,   0.99565810,
-              -5.28923943,  -0.90870027,  -2.40751854,  -2.44696544,  -6.52252202,
-              -6.24090500)
+trueValues[10] = trueValues[10] * 2
+trueValues[par_index$pi_logit] = c(-2,-2)
+# trueValues= c(-2.31617310,  -1.28756312,  -1.10116400,  -2.52367543,  -2.10384797,
+#               0.27050001, -11.65470594,  -0.49306415,   0.28862090,   0.22731278,
+#               -0.39079609,  -0.05894252,  -0.32509646,   0.48631653,   0.99565810,
+#               -5.28923943,  -0.90870027,  -2.40751854,  -2.44696544,  -6.52252202,
+#               -6.24090500)
 
 beta <- matrix(trueValues[par_index$beta], ncol = 3, byrow = F)
 initProbs_temp = c( 1, exp(trueValues[par_index$pi_logit][1]), 
@@ -111,19 +110,20 @@ for(it in 1:100) {
 
     par_est_split[[it]] = optim_coeff_split
     
-    # Using msm to compare parameter estimates ---------------------------------
+    # Using nhm to compare parameter estimates ---------------------------------
+    
 #     qmat <- matrix(c( 0,exp(-2),      0,exp(-2),
 # 	                  0,      0,exp(-2),exp(-2),
 # 	                  0,      0,      0,exp(-2),
 # 	                  0,      0,      0,      0), ncol=4, byrow=TRUE)
 # 	dimnames(qmat) <- list( c('Well', 'Mild','Severe','Death'), c('Well', 'Mild','Severe','Death'))
 # 
-#     Output_msm <- msm(state ~ years, subject=ptnum, data=cavData, qmatrix=qmat, 
-#                       covariates= ~ 1 + disc_time + sex, center=FALSE, 
+#     Output_msm <- msm(state ~ years, subject=ptnum, data=cavData, qmatrix=qmat,
+#                       covariates= ~ 1 + disc_time + sex, center=FALSE,
 #                       covinits=list(disc_time=c(beta[,2]),sex=beta[,3]),
-#                       deathexact=4)  
-#     
-#     par_est[[it]] = Output_msm$opt$par
+#                       deathexact=4)
+# 
+#     par_est[[it]] = c(Output_msm$opt$par)
     
 }
 
@@ -131,7 +131,7 @@ par_est_mat       = matrix(nrow = 100, ncol = 15)
 par_est_mat_split = matrix(nrow = 100, ncol = 15)
 
 for(i in 1:100) {
-    # par_est_mat[i,] = c(par_est[[i]])
+    par_est_mat[i,] = c(par_est[[i]])
     par_est_mat_split[i,] = c(par_est_split[[i]])
 }
 
@@ -167,8 +167,8 @@ pdf(pdf_title)
 VP <- vector(mode="list", length = length(labels))
 for(r in 1:length(labels)) {
     # Boxplots for par_est_mat
-    yVar = c(par_est_mat_split[,r])
-    disc_type = c(rep('split', nrow(par_est_mat_split)))
+    yVar = c(par_est_mat[,r], par_est_mat_split[,r])
+    disc_type = c(rep('msm', nrow(par_est_mat_split)), rep('AJ', nrow(par_est_mat_split)))
     x_label = paste0("Parameter Value: ", round(trueValues[r], 3))
     truth_par = trueValues[r]
     
--- AJ_Est_test/cav_simulate.r
+++ AJ_Est_test/cav_simulate.r
@@ -12,19 +12,19 @@ set.seed(num_iter)
 
 # Set the sample size.  Note that the true cav data set has 622 subjects.
 N <- 2000
-# Choose the discretization of time.
+# Choose the discretization for "instantaneous" time.
 dt <- 1/5000
 
 par_index = list( beta=1:15, misclass=16:19, pi_logit=20:21)
 
 load('DataOut/trueValues.rda')
-# trueValues[10] = trueValues[10] * 2
-# trueValues[par_index$pi_logit] = c(-2,-2)
-trueValues= c(-2.31617310,  -1.28756312,  -1.10116400,  -2.52367543,  -2.10384797,
-              0.27050001, -11.65470594,  -0.49306415,   0.28862090,   0.22731278,
-              -0.39079609,  -0.05894252,  -0.32509646,   0.48631653,   0.99565810,
-              -5.28923943,  -0.90870027,  -2.40751854,  -2.44696544,  -6.52252202,
-              -6.24090500)
+trueValues[10] = trueValues[10] * 2
+trueValues[par_index$pi_logit] = c(-2,-2)
+# trueValues= c(-2.31617310,  -1.28756312,  -1.10116400,  -2.52367543,  -2.10384797,
+#               0.27050001, -11.65470594,  -0.49306415,   0.28862090,   0.22731278,
+#               -0.39079609,  -0.05894252,  -0.32509646,   0.48631653,   0.99565810,
+#               -5.28923943,  -0.90870027,  -2.40751854,  -2.44696544,  -6.52252202,
+#               -6.24090500)
 
 betaMat <- matrix(trueValues[par_index$beta], ncol = 3, byrow = F)
 
